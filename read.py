4f174ed14ca99e7fdfc582c803eacb34776603ca
Manny <gmannygarcia@gmail.com>
2018-03-17 20:48:35 -0700

    fix linear/geometric mixup. Function is exponential due to having 2 for loops.

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ aviation_data = file[1:]
 
 def get_specific_airport_code(data, string_youre_after):
     """Find the row(s) of data where a specific string occurs"""
-    #Note: Below is a linear algorithm to find the specified string.
+    #Note: Below is a geometric algorithm to find the specified string.
     #   This approach is ideal for short lists. However, in this case
     #   it does not maximize efficieny, since the user risks
     #   iterating through the entire dataset before finding the code.
@@ -19,3 +19,16 @@ def get_specific_airport_code(data, string_youre_after):
     return(str_found)
 
 lax_code = get_specific_airport_code(data=aviation_data, string_youre_after='LAX94LA336')
+
+
+#linear time algorithm to search for string 'LAX94LA336'
+def linear_find_str(data_column, str='LAX94LA336'):
+    """Returns column name(s) and row number(s) where str is present"""
+    row_indexes = []
+    for row in range(0, len(data)-1):
+        for col in range(0, len(row)-1):
+            if str(data[row][col]) == str:
+                row_indexes.append(tuple(header[col], row))
+    return(row_indexes)
+
+lin_str = linear_find_str(data=aviation_data, str='LAX94LA336')
