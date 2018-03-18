2e157d87c545eb71565471ee556abe546a3ac149
Manny <gmannygarcia@gmail.com>
2018-03-17 20:50:54 -0700

    Add linear function. Update comments to clarifiy algorithmic time differences between this and previous function.

--- read.py
+++ read.py
@@ -6,7 +6,10 @@ header = file[0]
 aviation_data = file[1:]
 
 def get_specific_airport_code(data, string_youre_after):
-    """Find the row(s) of data where a specific string occurs"""
+    """
+    Find the row(s) of data where a specific string occurs
+    using an exponential time algorithm.
+    """
     #Note: Below is a geometric algorithm to find the specified string.
     #   This approach is ideal for short lists. However, in this case
     #   it does not maximize efficieny, since the user risks
@@ -22,13 +25,11 @@ lax_code = get_specific_airport_code(data=aviation_data, string_youre_after='LAX
 
 
 #linear time algorithm to search for string 'LAX94LA336'
-def linear_find_str(data_column, str='LAX94LA336'):
-    """Returns column name(s) and row number(s) where str is present"""
-    row_indexes = []
-    for row in range(0, len(data)-1):
-        for col in range(0, len(row)-1):
-            if str(data[row][col]) == str:
-                row_indexes.append(tuple(header[col], row))
-    return(row_indexes)
+def linear_find_str(data, string_youre_after):
+    """
+    Find the row(s) of data where a specific string occurs
+    using a linear time algorithm.
+    """
+
 
 lin_str = linear_find_str(data=aviation_data, str='LAX94LA336')
