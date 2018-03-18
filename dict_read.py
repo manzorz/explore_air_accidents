4022ff4533ca55e18ddf9cce0f8b0033c415b236
Manny <gmannygarcia@gmail.com>
2018-03-17 23:27:42 -0700

    search dictionaries for specific string

--- dict_read.py
+++ dict_read.py
@@ -4,7 +4,24 @@ with open('AviationData.txt', 'r') as file:
 file = [row.split(' | ') for row in lines]
 header = file[0]
 aviation_data = file[1:]
-len(header)
 
 aviation_dict_list = []
-for l in aviation_data
+for l in aviation_data:
+    d = dict(zip(header, l))
+    aviation_dict_list.append(d)
+
+def string_search_list_of_dicts(list_of_dicts, string):
+    """
+    Iterates through a list of dictionaries and
+    through those dictionaries themselves to
+    find a specific string in the values
+
+    Exponential time function, two for loops
+    """
+    for d in list_of_dicts:
+        for key,value in d.items():
+            if value == string:
+                return(d)
+
+lax_dict = string_search_list_of_dicts(aviation_dict_list, 'LAX94LA336')
+lax_dict
